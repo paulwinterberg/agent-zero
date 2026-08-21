b4453e63470bcd150dfa5e5e2c0bbe9a0c4a3fb2
paulwinterberg <paul.winterberg@jfks.me>
2026-08-21 09:23:09 +0000

    Updates to gameplay

--- main/states/gameplay.py
+++ main/states/gameplay.py
@@ -8,7 +8,7 @@ from core.renderer import load_tilemap, render_world
 class Gameplay(State):
     def __init__(self):
         self.player = Player()
-        self.tilemap, self.group = load_tilemap("assets\\levels\\hq_lobby.tmx", self.player)
+        self.tilemap, self.group = load_tilemap("assets/levels/hq_lobby.tmx", self.player)
         
         self.tilemap.zoom_to(4)
         
